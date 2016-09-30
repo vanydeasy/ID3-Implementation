74529d9f1730bd741679853bc0b7ccda2b1fa252
Venny Larasati <venny_la@yahoo.com>
2016-09-30 23:30:01 +0700

    save and load model

--- MyClassifier/src/myclassifier/MyClassifier.java
+++ MyClassifier/src/myclassifier/MyClassifier.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.trees.Id3;
@@ -27,6 +28,7 @@ public class MyClassifier {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, Exception {
+        // Load data from ARFF or CSV
         Instances data;
         if (args[0].substring(args[0].lastIndexOf(".") + 1).equals("arff")){
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
@@ -38,13 +40,21 @@ public class MyClassifier {
             data = loader.getDataSet();
         }
         
+        // Build Naive Bayes Model
         data.setClassIndex(data.numAttributes() - 1);
         NaiveBayes model = new NaiveBayes();
         model.buildClassifier(data);
         System.out.println(model.toString());
         
+        // Save Model
+        weka.core.SerializationHelper.write(args[0].substring(0, args[0].indexOf('.')) + "_naivebayes.model", model);
+        
+        // Load Model
+        Classifier cls = (Classifier) weka.core.SerializationHelper.read(args[0].substring(0, args[0].indexOf('.')) + "_naivebayes.model");
+        
+        // 10-fold Cross Validation Evaluation
         Evaluation eval = new Evaluation(data);
-        eval.crossValidateModel(model, data, 10, new Random());
+        eval.crossValidateModel(cls, data, 10, new Random());
         System.out.println(eval.toSummaryString("\n\n\n\nNaive Bayes 10-Fold Cross Validation\n============\n", false));
     }
     
