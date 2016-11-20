87a07779f52e0182109247374c53699e07134c3c
Venny Larasati <venny_la@yahoo.com>
2016-11-20 17:14:36 +0700

    Add functions to MyKMeans

--- MyClassifier/src/myclusterer/MyKMeans.java
+++ MyClassifier/src/myclusterer/MyKMeans.java
@@ -8,6 +8,8 @@ package myclusterer;
 import weka.clusterers.*;
 import weka.core.Capabilities;
 import weka.core.CapabilitiesHandler;
+import weka.core.DistanceFunction;
+import weka.core.EuclideanDistance;
 import weka.core.Instance;
 import weka.core.Instances;
 
@@ -16,16 +18,42 @@ import weka.core.Instances;
  * @author Venny
  */
 public class MyKMeans implements Clusterer, CapabilitiesHandler {
+    private DistanceFunction distanceFunction = new EuclideanDistance();
     private int numClusters = 2;
+    private int maxIterations = 500;
+    private Instances centroids = null;
     
     @Override
-    public void buildClusterer(Instances i) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void buildClusterer(Instances instances) throws Exception {
+        // INITIALIZATION:
+        boolean convergence = false;
+        int numIterations = 0;
+        // Pick centroids randomly
+        
+        // While convergence == false AND numIterations < maxIterations
+            // ASSIGNMENT:
+            // For each instances as i
+                // clusterInstance(i)
+
+            // UPDATE CENTROID:
+            // For each cluster
+                // Calculate new centroid
+            // If old centroids = new centroids
+                // convergence = true;
+            
     }
 
     @Override
-    public int clusterInstance(Instance instnc) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int clusterInstance(Instance instance) throws Exception {
+        int centroid = 0;
+        double distance = 0;
+        // For each centroids
+            // Calculate distance
+            // if current distance < prev distance
+                // centroid = current centroid
+                // distance = current distance
+        
+        return centroid;
     }
 
     @Override
@@ -48,7 +76,37 @@ public class MyKMeans implements Clusterer, CapabilitiesHandler {
         return result;
     }
     
-    public void setNumClusters(int numClusters) {
+    public int getNumClusters() {
+        return this.numClusters;
+    }
+    
+    public void setNumClusters(int numClusters) throws Exception {
+        if (numClusters <= 0) {
+            throw new Exception("Number of clusters must be > 0");
+        }
         this.numClusters = numClusters;
+    }
+    
+    public int getMaxIterations() {
+        return this.maxIterations;
+    }
+     
+    public void setMaxIterations(int max) throws Exception {
+        if (max <= 0) {
+            throw new Exception("Maximum number of iterations must be > 0");
+        }
+        this.maxIterations = max;
+    }
+    
+    public DistanceFunction getDistanceFunction() {
+        return this.distanceFunction;
+    }
+    
+    public void setDistanceFunction(DistanceFunction distanceFunction) {
+        this.distanceFunction = distanceFunction;
+    }
+    
+    public Instances getClusterCentroids() {
+        return this.centroids;
     }
 }
